8d33f41f295308bfb09fa218bc46a1e05ef8312a
EricThomson <thomson.eric@gmail.com>
2021-03-01 20:38:30 -0500

    added median_semed function

--- anaties/stats.py
+++ anaties/stats.py
@@ -8,6 +8,12 @@ import numpy as np
 
 
 
+def med_semed(array, axis = None):
+    """ 
+    return median and std error of the median or numpy array
+    """
+    return np.median(array, axis = None), se_median(array, axis = None)
+
 
 def mn_sem(array, axis = None):
     """
@@ -21,13 +27,6 @@ def mn_std(array, axis = None):
     return mean and std deviation of numpy array 
     """
     return np.mean(array, axis = axis), np.std(array)
-
-
-def med_semed(array, axis = None):
-    """ 
-    return median and std error of the median or numpy array
-    """
-    return np.median(array, axis = None), se_median(array, axis = None)
 
 
 def se_mean(array, axis = None):
